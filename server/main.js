0e877ee1413beb44db5979d1844487d9b6f29b49
davidlewallen <lewallen.david@gmai.com>
2016-07-07 23:34:55 -0600

    Fixed server/main.js to work with browserify

--- server/main.js
+++ server/main.js
@@ -1,6 +1,7 @@
 var express    = require('express');
 var path       = require('path');
 var browserify = require("browserify-middleware");
+
 var bodyParser = require('body-parser');
 
 var app        = express();
@@ -11,7 +12,7 @@ app.use(express.static(path.join(__dirname, "../client/public")));
 // When loading the webpage, we will make a request to GET /app-bundle.js which is the bundled .js files
 // We then transform all the code with babelify so that any react/es2015 code can be interpreted with es5 standards 
 app.get('/app-bundle.js',
- browserify('./client/main.js', {
+ browserify('../client/main.js', {
     transform: [ [ require('babelify'), { presets: ["es2015", "react"] } ] ]
   })
 );
