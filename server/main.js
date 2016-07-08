b172a669864e4ab91a1e85d8dfbbc7a26bd0dd73
Jordan Campbell <jordan@ignitionstudios.net>
2016-07-08 11:28:20 -0500

    Working on signup

--- server/main.js
+++ server/main.js
@@ -1,12 +1,16 @@
 var express    = require('express');
 var path       = require('path');
 var browserify = require("browserify-middleware");
+var Utils      = require('Utils');
+
+var db         = require('./db.js');
 
 var bodyParser = require('body-parser');
 
 var app        = express();
 
 app.use(express.static(path.join(__dirname, "../client/public")));
+app.use(bodyParser.json());
 
 // This will bundle all of our .js files into one.
 // When loading the webpage, we will make a request to GET /app-bundle.js which is the bundled .js files
@@ -17,7 +21,34 @@ app.get('/app-bundle.js',
   })
 );
 
+app.post('/signup', function(req, res) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  db.collection('users').find({username: username})
+  .then((user) => {
+    console.log(user);
+    if(user[0]){
+      res.end(400, "bad request");
+    } else {
+      return Utils.hashPassword(password)
+    }
+  })
+  .then(function(hash){
+    return db.collection('users').insert({username: username, password: hash});
+  })
+  .then(function(obj){
+    console.log("Data returned from inserting:", obj);
+    var sessionId = Utils.createSessionId();
+    return db.collection('sessions').insert({username: username, sessionId: sessionId});
+  })
+  .then(function(){
+    res.end();
+  })
+})
+
 app.post('/login', function(req, res) {
+  req.body
   /*TODO: add functionality for POST /login
     Takes a username, password
     Checks to see if the user is in the system
---
+++ server/utils.js
@@ -0,0 +1,29 @@
+var bcrypt = require('bcrypt-nodejs');
+var uuid = require('uuid-js');
+
+var Utils = {
+  hashPassword: function(password) {
+    return new Promise(function(resolve, reject){
+      bcrpyt.hash(password, null, null, function(err, hash){
+        if(err) console.log("bcrpyt error:", err);
+          resolve(hash); 
+      })
+    })
+  },
+
+  comparePassword: function(hash, attemptedPassword) {
+    return new Promise(function(resolve, reject){
+      bcrpyt.compare(attemptedPassword, hash, function(err, isCorrect){
+        if(err) console.log("bcrpyt error:", err);
+          resolve(isCorrect);             
+      })
+    })
+  },
+
+  createSessionId: function() {
+    return uuid.v4();
+  }
+}
+
+modules.export = Utils;
+
