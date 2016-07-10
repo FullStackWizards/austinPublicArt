5696431a407d976d9d8f7bd2c604ee709d973a74
Caleb Anderson <caleb.anderson130@gmail.com>
2016-07-10 18:28:07 -0500

    Implement on-click info modal

--- client/components/ArtGallery.js
+++ client/components/ArtGallery.js
@@ -1,36 +1,40 @@
 import React from 'react'
-
+import {ModalContainer, ModalDialog} from 'react-modal-dialog';
+import ReactSpinner from 'react-spinjs';
+// import PictureShow from 'react-picture-show'
 
 export default class ArtGallery extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      artCollection: []
+      artCollection: [],
+      showInfo: false
     }
-  }
-
-  checkState() {
-    console.log("this.props.gallery", this.props.gallery)
-  }
-  componentWillMount() {
-    this.props.fetchArt()
-    setTimeout(this.checkState.bind(this), 5000)
   }
 
   parseImageUrl(imgUrl) {
     imgUrl = imgUrl.split(';')
-    return imgUrl[0]
+    return imgUrl
+  }
+
+  openInfo(art) {
+    this.setState({showInfo: true});
+    this.setState({currentArt: art})
+  }
+  closeInfo() {
+    this.setState({showInfo: false});
   }
 
   render() {
     return (
       <div className="artGallery">
-        { this.props.gallery ? this.props.gallery.map((art) => {
+        {this.state.showInfo ?
+          <Info onClose={this.closeInfo.bind(this)} currentArt={this.state.currentArt} parseImageUrl={this.parseImageUrl.bind(this)}/>
+        : null} 
+        {this.props.gallery ? this.props.gallery.map((art) => {
           return <div className="artwork" key={art._id}>
-            <img className="artImage" src={this.parseImageUrl(art.Images)} />
-            <p className="artTitle"> {art['Art Title']} </p>
-            <p className="artistName"> {art['Artist Name']} </p>
+            <img className="artImage" src={this.parseImageUrl(art.Images)[0]} onClick={(e) => this.openInfo(art)}/>            
             {this.props.loggedIn ?
             <div className="userFeatures">
             <button>Like</button>
@@ -43,3 +47,30 @@ export default class ArtGallery extends React.Component {
     )
   }
 }
+
+
+
+
+class Info extends React.Component {
+
+ 
+  render() {
+    return <ModalContainer onClose={this.props.onClose}>
+     
+        <ModalDialog onClose={this.props.onClose} className="info">
+         
+            <h2>{this.props.currentArt['Art Title']}</h2>
+            <p>By: {this.props.currentArt['Artist Name']}</p>     
+            <img src={this.props.parseImageUrl(this.props.currentArt.Images)[0]} />
+           
+        </ModalDialog>
+      
+    </ModalContainer>;
+  }
+}
+
+
+
+
+
+
