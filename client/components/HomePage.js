fbd8f0e37b874a02a3893a0c5b1e32e0f31bada0
Caleb Anderson <caleb.anderson130@gmail.com>
2016-07-11 14:36:17 -0500

    changed homepage

--- client/components/HomePage.js
+++ client/components/HomePage.js
@@ -7,12 +7,7 @@ export default class HomePage extends React.Component {
   render() {
     return (
       <div>
-        <div>
         <NavBar />
-        </div>
-        <div >
-          <h2>HomePage</h2>
-        </div>
       </div>
     )
   }
