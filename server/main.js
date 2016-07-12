992acf3329dd952a87b65d2773f4c993ad110e11
David <lewallen.david@gmail.com>
2016-07-11 23:27:10 -0500

    Implementing POST/favorites - Researching how to check multiple fields at the same time

--- server/main.js
+++ server/main.js
@@ -11,10 +11,6 @@ var app        = express();
 app.use(express.static(path.join(__dirname, "../client/public")));
 app.use(bodyParser.json());
 
-// This will bundle all of our .js files into one.
-// When loading the webpage, we will make a request to GET /app-bundle.js which is the bundled .js files
-// We then transform all the code with babelify so that any react/es2015 code can be interpreted with es5 standards 
-
 app.get('/app-bundle.js',
 browserify(path.join(__dirname, '../client/main.js'), {
    transform: [ [ require('babelify'), { presets: ["es2015", "react"] } ] ]
@@ -62,25 +58,61 @@ app.post('/login', function(req, res) {
 
  var userID;
 
-  db.users.find({username: username})
-  .then((userObj) => {
-    if(!userObj) {
-      res.end(400, "Invalid username/Password")
-    } else {
-      userID = userObj[0]._id;
-      return Utils.comparePassword(userObj[0].password, password)
-    }
-  })
-  .then((isValidPassword) => {
-    if(!isValidPassword) {
-      res.end(401, "Invalid Username/password");
-    } else {
-      return db.sessions.insert({id: userID, sessionId: Utils.createSessionId()})
-    }
-  })
-  .then((userObj) => {
-    res.send(JSON.stringify(userObj.sessionId))
-  })
+  db.collection('users')
+    .find({username: username})
+    .then((userObj) => {
+      if(!userObj) {
+        res.end(400, "Invalid username/Password")
+      } else {
+        userID = userObj[0]._id;
+        return Utils.comparePassword(userObj[0].password, password)
+      }
+    })
+    .then((isValidPassword) => {
+      if(!isValidPassword) {
+        res.end(401, "Invalid Username/password");
+      } else {
+        return db.collection('sessions')
+                 .insert({ id: userID, sessionId: Utils.createSessionId() })
+      }
+    })
+    .then((userObj) => {
+      res.send(JSON.stringify(userObj.sessionId))
+    })
+})
+/*
+  Data In: Art ID
+           Headers will have cookie
+
+  Create Favorites collection
+    -> User ID
+    -> Art ID
+
+  If we add total amount of favorites we also need to increment the favorite key of that specific art in the Art Collection
+
+  1) Check UUID to see if it is in the Sessions, half ass authentication
+    1) True: Grab _id for the UUID
+      1) Add an entry to Favorites collection with _id and art ID
+    2) False: return error 403
+  2) Return something here if success or error if fail
+
+ */
+app.get('/favorites', function(req, res) {
+  var artId = req.body.artId;
+  var sessionId = req.headers.cookie.substring(10);
+  var userID;
+
+  db.collection('sessions')
+    .find({sessionId: sessionId})
+    .then((returnedSession) => userId = returnedSession[0]._id)
+    .then(() => {
+      db.collection('favorites').find({ _id: userID, ar})
+      db.collection('favorites')
+      .insert({ userId: userID, artId: artId})
+    .then(() => db.collection('art')
+      .update({ _id: artId }, { $inc: { favorites: 1 } }))
+  
+  res.end();
 })
 
 // Run server on port 4040
