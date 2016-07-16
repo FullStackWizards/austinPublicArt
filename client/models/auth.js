4fbb5bc6133a478eb66c970ffdeb3b1e606b065f
David <lewallen.david@gmail.com>
2016-07-16 14:30:16 -0500

    Removed console.logs and removed commented out code

--- client/models/auth.js
+++ client/models/auth.js
@@ -2,7 +2,6 @@ import fetch from 'isomorphic-fetch';
 
 
 export function signUp(userData) {
-  console.log('signingup in auth.js~~~~~', userData)
   let obj = {
     method: "POST",
     headers: {
@@ -12,7 +11,6 @@ export function signUp(userData) {
   }
   return fetch(`/signUp`, obj)
     .then(function(data){
-      console.log('after login data', data)
       if(data.status < 400) {
         data.json().then((data) => document.cookie = "sessionId=" + data + ";path=/")
         return "Success"
@@ -22,7 +20,6 @@ export function signUp(userData) {
 
 
 export function login(userData) {
-	console.log('logging in auth.js~~~~~', userData)
 	let obj = {
     method: "POST",
     headers: {
@@ -32,7 +29,6 @@ export function login(userData) {
   }
   return fetch(`/login`, obj)
     .then(function(data){
-      console.log('after login data', data)
       if(data.status < 400) {
         data.json().then((data) => document.cookie = "sessionId=" + data + ";path=/")
         return "Success"
@@ -41,7 +37,6 @@ export function login(userData) {
 }
 
 export function likePhoto(artId) {
-  console.log('liking in auth.js~~~~~', artId, document.cookie)
   let obj = {
     method: "POST",
     headers: {
@@ -56,7 +51,6 @@ export function likePhoto(artId) {
 }
 
 export function favoritePhoto(artId) {
-  console.log('favoriting photo', artId)
   let obj = {
     method: "POST",
     headers: {
@@ -66,7 +60,6 @@ export function favoritePhoto(artId) {
   }
   return fetch(`/favorites/${artId}`, obj)
     .then(function(data){
-      console.log('data in auth.js', data)
       return data
     })
 }
@@ -76,14 +69,11 @@ export function fetchFavs() {
     method: "GET",
     headers: {
       'Content-Type': 'application/json',
-      //'cookie': JSON.stringify(document.cookie)
       'cookieHeader': document.cookie
     }
-    //body: JSON.stringify({ cookie: document.cookie })
   }
 return fetch(`/favorites`, obj)
   .then(function(resp) {
-    console.log('response', resp)
     return resp
   })
 }
