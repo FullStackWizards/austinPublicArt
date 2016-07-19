8524651fff5416f883f021dc79103d1aa14038d0
Tom LeConey <tleconey@gmail.com>
2016-07-19 18:59:01 -0500

    added app.use for basic passport

--- server/main.js
+++ server/main.js
@@ -19,8 +19,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Load Routes
 routes(app);
+
 
 
 app.use(history());
