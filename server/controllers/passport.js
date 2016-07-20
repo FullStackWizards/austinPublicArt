a371fd04ee29807051ce7448d66904566b929e2e
Tom LeConey <tleconey@gmail.com>
2016-07-20 15:45:45 -0500

    still working in passport.js

--- server/controllers/passport.js
+++ server/controllers/passport.js
@@ -32,12 +32,32 @@ module.exports = function (passport) {
 					newUser.local.username = emial;
 					newUser.local.password = newUser.generaateHash(password);
 
-					newUser.save(fun)
+					newUser.save(function(err){
+						if(err)
+							throw err;
+						return done(null, newUser);
+					})
 				}
+			})
+		});
+	}));
+
+	passport.user('local-login', new LocalStrategy({
+		usernameField: 'email',
+		passwordField: 'password',
+		passReqToCallback: true
+	},
+	function(req, email, password, done){
+		process.nextTick(function(){
+			User.findOne({'local.username': email}, function(err, user){
+				if(err)
+					return done(err);
+				if(!user)
+					return done(null, false, req.flash('loginMessage', 'No user sdjfnskdn'))
+				if(!user.valid)
 			})
 		})
 	}
-
 	}))
 
 
