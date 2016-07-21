2bf960cfb95707dd219de5112e08ffd0f84c806e
Tom LeConey <tleconey@gmail.com>
2016-07-21 09:49:06 -0500

    substantial restructuring of auth.js inn progress

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,18 +1,97 @@
-// var User = require('./models/user');
+var express = require('express');
+var passport   = require("passport");
+var LocalStrategy   = require('passport-local').Strategy;
 
-module.exports = function(app, passport){
-  // app.get('/', function(req, res){
-  //   res.render('index.ejs');
+var Favorite = require('../models/favorite');
+var Utils = require(path.join(__dirname, '../utils.js'));
+var db = require(path.join(__dirname, '../db.js'));
+
+var router = express.Router();
+module.exports = router;
+
+
+//////////////////// REDONE //////////////////////////
+
+router.post('/signup', passport.authenticate('local-signup'), function(req, res) {
+    console.log("auth.js line 51-passport sign up")
+    // successRedirect: '/',
+    // failureRedirect: '/signup',
+    // failureFlash: true
+    var username = req.body.username;
+    var password = req.body.password;
+    console.log("req",req.sessionID)
+    console.log("res",res.body)
+  
+    res.send(JSON.stringify(obj.sessionId));
+  })
+
+// router.post('/signUp', function(req, res) {
+//   const username = req.body.username;
+//   const password = req.body.password;
+
+//   Auth.getUser(username)
+//   .then(user => {
+//     if(user[0]){
+//       res.statusMessage = "Username taken."
+//       res.status(400).end();
+//     } else {
+//       return Auth.signUp(username, password);
+//     }
+//   })
+//   .then(user =>
+//     Auth.createSession(user._id)
+//   )
+//   .then(session => {
+//     res.send(JSON.stringify(session.sessionId));
+//   })
+// })
+
+//////*******//////
+//////*******//////
+//////*******//////
+//////*******//////
+//////*******//////
+//////*******//////
+
+router.post('/login', passport.authenticate('local-login', function(req, res)
+  {
+    var username = req.body.username;
+    var password = req.body.password;
+    var userID;
+     db.collection('users')
+    .find({username: username})
+    .then((userObj) => {
+      if(!userObj[0]) {
+        res.statusMessage = "Incorrect username or password"
+        res.status(400).end();
+      } else {
+        userID = userObj[0]._id;
+        return Utils.comparePassword(userObj[0].password, password)
+      }
+    })
+    .then((isValidPassword) => {
+      if(!isValidPassword) {
+        res.statusMessage = "Incorrect username or password"
+        res.status(401).end();
+      } else {
+        return db.collection('sessions')
+          .insert({ id: userID, sessionId: Utils.createSessionId() })
+      }
+    })
+    .then((userObj) => {
+      res.send(JSON.stringify(userObj.sessionId))
+    })
+})
+
+
+  // // app.get('/', function(req, res){
+  // //   res.render('index.ejs');
+  // // });
+
+  // app.get('/login', function(req, res){
+  //   res.render('login.ejs', { message: req.flash('loginMessage') });
   // });
-
-  app.get('/login', function(req, res){
-    res.render('login.ejs', { message: req.flash('loginMessage') });
-  });
-  app.post('/login', passport.authenticate('local-login', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-  }));
+  
 
 //   router.post('/login', function(req, res) {
 //   const username = req.body.username;
@@ -42,37 +121,27 @@ module.exports = function(app, passport){
 //     })
 // })
 
+
+//////*******//////
+//////*******//////
+//////*******//////
+//////*******//////
+//////*******//////
+//////*******//////
+
+
+
+
+
+
+
+
   app.get('/signup', function(req, res){
     res.render('signup.ejs', { message: req.flash('signupMessage') });
   });
 
 
-  app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect: '/',
-    failureRedirect: '/signup',
-    failureFlash: true
-  }));
-
-// router.post('/signUp', function(req, res) {
-//   const username = req.body.username;
-//   const password = req.body.password;
-
-//   Auth.getUser(username)
-//   .then(user => {
-//     if(user[0]){
-//       res.statusMessage = "Username taken."
-//       res.status(400).end();
-//     } else {
-//       return Auth.signUp(username, password);
-//     }
-//   })
-//   .then(user =>
-//     Auth.createSession(user._id)
-//   )
-//   .then(session => {
-//     res.send(JSON.stringify(session.sessionId));
-//   })
-// })
+  
 
 
   app.get('/profile', isLoggedIn, function(req, res){
