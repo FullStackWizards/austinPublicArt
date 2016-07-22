c4f9b8151d81fc9a96097649a295332ecb5d912e
Jad Carson <Jad.Carson@gmail.com>
2016-07-22 10:04:31 -0500

    forgot to remove a git conflict line

--- server/db.js
+++ server/db.js
@@ -1,10 +1,8 @@
 var pmongo = require('promised-mongo');
 
-<<<<<<< c370a02d5173c88b14ac1027ea65d69d0fd30c6c
+
 var uri = 'mongodb://jad:carson@ds023485.mlab.com:23485/austin_art';
-=======
-var uri ='mongodb://jad:carson@ds023485.mlab.com:23485/austin_art';
->>>>>>> Have a working map and now adding extra info to it
+
 
 var db = pmongo(uri, {
   authMechanism: 'ScramSHA1'
