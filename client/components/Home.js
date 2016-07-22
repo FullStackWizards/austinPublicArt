fd2e09a948dc8815e486f0c60323094684b64e61
Kenneth Torng <ktorng@gmail.com>
2016-07-21 23:14:36 -0500

    Style Home Page font and buttons

--- client/components/Home.js
+++ client/components/Home.js
@@ -9,19 +9,31 @@ export default class Home extends React.Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 col-sm-12">
-                <h1 className="wow fadeInDown">AUSTIN ART</h1>
+                <h1 className="wow fadeInDown title">AUSTIN ART</h1>
                 <Link 
                   to={`/gallery`} 
-                  className="btn btn-danger wow fadeInUp" 
+                  className="button"
                   data-wow-delay="0.4s"
                 >
+                  <span aria-hidden="true">
+                    <span className="line left"></span>
+                    <span className="line top"></span>
+                    <span className="line right"></span>
+                    <span className="line bottom"></span>
+                  </span>
                   GALLERY
                 </Link>
                 <Link 
                   to={`/artists`} 
-                  className="btn btn-default smoothScroll wow fadeInUp" 
+                  className="button"
                   data-wow-delay="0.6s"
                 >
+                  <span aria-hidden="true">
+                    <span className="line left"></span>
+                    <span className="line top"></span>
+                    <span className="line right"></span>
+                    <span className="line bottom"></span>
+                  </span>
                   ARTISTS
                 </Link>       
               </div>
